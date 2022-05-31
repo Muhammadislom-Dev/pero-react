814c8011e033479d04380785c235b2682d35aaaf
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-31 20:08:53 +0500

    Initial commit

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+import Company from '../../components/Company/Company'
+
+const Home = () =>{
+    return(
+        <div className="home">
+            <Company />
+        </div>
+    )
+}
+export default Home
