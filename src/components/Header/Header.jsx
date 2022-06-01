1f7ca8211fee69e418fbbbb1b510d73906d2e800
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-01 20:20:47 +0500

    update files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,19 @@ import guarantee from '../../Assets/img/guarantee.png'
 import lock from '../../Assets/img/lock.png'
 import milk from '../../Assets/img/milk.png'
 
+import mgreen from '../../Assets/img/mgreen.png'
+import mgren from '../../Assets/img/mgren.png'
+
+import Vector from '../../SVG/Vector'
+import Lock from '../../SVG/Lock'
+import Check from '../../SVG/Check'
+
+
 const Header = () =>{
+
     return(
         <div className="header">
-            <div className="container">
+            <div className="container ">
                 <div className="header-page">
                     <div className="header-left">
                         <h2 className="header-name">Универсальные <span className="header-names">салфетки</span></h2>
@@ -17,15 +26,24 @@ const Header = () =>{
                           
                         <ul className="header-list">
                             <li className="header-item">
-                                <div className="header-span"><img src={vector} alt="" className="header-img" /></div>
+                                <button className="header-span">
+                                    <Vector />
+                                    {/* <img src={vector} alt="" className="header-img" /> */}
+                                </button>
                                 <p className="header-text">Плотность и мягкость</p>
                             </li>
                             <li className="header-item">
-                                <div className="header-span"><img src={guarantee} alt="" className="header-imgs" /></div>
+                                <button className="header-span">
+                                    <Check />
+                                    {/* <img src={guarantee} alt="" className="header-imgs" /> */}
+                                </button>
                                 <p className="header-text">Гарантия высокого качества</p>
                             </li>
                             <li className="header-item">
-                                <div className="header-span"><img src={lock} alt="" className="header-img" /></div>
+                                <button className="header-span">
+                                    <Lock className="header-img" />
+                                    {/* <img src={lock} alt="" className="header-img" /> */}
+                                </button>
                                 <p className="header-text">Безопасность продукта</p>
                             </li>
                         </ul>
@@ -50,6 +68,9 @@ const Header = () =>{
                     </div>
                 </div>
             </div>
+
+            <img src={mgreen} alt="" className="header-green" />
+            <img src={mgren} alt="" className="header-gren" />
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,46 +2,53 @@ import React from 'react'
 import './Navbar.css'
 
 import pero from '../../Assets/img/pero.png'
-import call from '../../Assets/img/call.png'
+import call from '../../Assets/img/calls.png'
 import toggle from '../../Assets/img/toggle.png'
 
+import Toogle from '../../SVG/Toogle'
+
 const Navbar = () =>{
-    return(
-        <div className="navbar">
-            <div className="container">
-                <div className="navbar-page">
-                    <div className="navbar-box">
-                        <img src={pero} alt="" className="navbar-img" />
-                        <div className="navbar-titles">
-                            <img src={toggle} alt="" className="navbar-logo" />
-                            <p className="navbar-name">Наша продукция</p>
-                        </div>
-                    </div>
-                    <ul className="navbar-list">
-                        <li className="navbar-item">
-                            <a href="#" className="navbar-link">Гдавная</a>
-                        </li>
-                        <li className="navbar-item">
-                            <a href="#" className="navbar-link"> О Компании</a>
-                        </li>
-                        <li className="navbar-item">
-                            <a href="#" className="navbar-link">НoReCa</a>
-                        </li>
-                        <li className="navbar-item">
-                            <a href="#" className="navbar-link">Tissue</a>
-                        </li>
-                        <li className="navbar-item">
-                            <a href="#" className="navbar-link">Контакты</a>
-                        </li>
-                    </ul>
-                    <div className="navbar-title">
-                        <img src={call} alt="" className="navbar-logos" />
-                        <a href="+998911770077" className="navbar-phone">(91) 177 00 77</a>
-                    </div>
-                </div>
+
+
+return(
+<div className="navbar">
+    <div className="container navbar--container">
+        <div className="navbar-page">
+            <div className="navbar-box">
+                <img src={pero} alt="" className="navbar-img" />
+                <button className="navbar-titles">
+                    {/* <img src={toggle} alt="" className="" /> */}
+                    <Toogle />
+                    Наша продукция
+                </button>
+            </div>
+            <ul className="navbar-list">
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">Главная</a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link"> О Компании</a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">НoReCa</a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">Tissue</a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">Контакты</a>
+                </li>
+            </ul>
+            <div className="navbar-title">
+                <img src={call} alt="" className="navbar-logos" />
+                <a href="+998911770077" className="navbar-phone">(91) 177 00 77</a>
             </div>
         </div>
-    )
+    </div>
+
+    {/* <a href="" className="main__button">Watch now</a> */}
+</div>
+)
 }
 
 export default Navbar
