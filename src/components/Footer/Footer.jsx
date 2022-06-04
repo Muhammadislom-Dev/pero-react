617418819cb96f193f9285bf143068ce731c004e
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-04 18:15:18 +0500

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import './Footer.css'
 
 import pero from '../../Assets/img/Frame.png'
+import abbacoding from '../../Assets/img/abbacoding.png'
+import abbamedia from '../../Assets/img/abbamedia.png'
 
 const Footer = () =>{
     return(
-        <div className="footer">
+        <>
+         <div className="footer">
             <div className="container">
                 <div className="footer-page">
                     <div className="footer-list">
@@ -87,19 +90,25 @@ const Footer = () =>{
                         </li>
                     </ul>
                 </div>
-                {/* <ul className="footer-title">
-                    <li className="footer-items">
-                        <i className="fa fa-facebook footer-icon" aria-hidden="true"></i>
-                    </li>
-                    <li className="footer-items">
-                        <i className="fa fa-twitter footer-icon" aria-hidden="true"></i>
-                    </li>
-                    <li className="footer-items">
-                        <i className="fa fa-instagram footer-icon" aria-hidden="true"></i>
-                    </li>
-                </ul> */}
+
+                <div className="footer-boxs">
+                    <div>
+                      <a href="#"><img src={abbacoding} alt="" className="footer-abba" /></a>
+                      <a href="#"><img src={abbamedia} alt="" className="footer-abba" /></a>
+                    </div>
+                    <div>
+                        <a href="#"><img src={abbacoding} alt="" className="footer-abba" /></a>
+                        <a href="#"><img src={abbamedia} alt="" className="footer-abba" /></a>
+                    </div>
+                </div>
             </div>
         </div>
+            <div className="footer-box">
+                <div className="container">
+                    <h4 className='footer-names'>Copyright PERO | Designed by <span className='footer-span'>abba marketing</span>   -Powered by <span className='footer-span'>ABBA</span></h4>
+                </div>
+            </div>
+        </>
     )
 }
 export default Footer
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,110 @@
 import './Form.css'
+import * as React from 'react';
+import { useState } from "react"
+// import Modal from '../Modal/Modal';
 
 const Form = () =>{
+
+    const [greatModal, setGreatModal] =  useState(false);
+
+    function openGreatModal(){
+        setGreatModal(!greatModal)
+    }  
+    
+    const formBtn = (e)=>{
+ 
+        e.preventDefault();
+            if (e.target[0].value.length > 0
+                 && e.target[1].value.length > 0 && e.target[2].value.length > 0
+                   ) {
+    
+                let botMessege = `
+                     Assalomu allaykum, Sizga Yangi Xabar!😊%0A
+                     Ismi 👤: ${e.target[0].value}%0A
+                     Raqam ☎: ${e.target[1].value}%0A 
+                     Xabar ✏️ ${e.target[2].value}%0A              
+                `;
+
+                let url =`https://api.telegram.org/bot5136951868:AAEbk5iaJ1l89mI2H7LXacKLS3ZcMbLHkAE/sendMessage?chat_id=-1001328953154&text=${botMessege}`;
+
+                async function fetchAsync(url) {
+                    let response = await fetch(url);
+                    let data = await response.json();
+
+                    return data;
+                   
+                }
+                fetchAsync(url);
+
+                if(document.querySelector("#name").matches(".input-error")){
+                    document.querySelector("#name").classList.remove("input-error")
+                    
+                    document.querySelector("#errorText").classList.remove("error-text1")
+                }
+                if(document.querySelector("#tel").matches(".tel-error")){
+                    document.querySelector("#tel").classList.remove("tel-error")
+                     document.querySelector("#errorTel").classList.remove("error-tel1")
+                }
+             
+                if(document.querySelector("#textarea").matches(".error-info")){
+                    document.querySelector("#textarea").classList.remove("error-info")
+                    document.querySelector("#errorInfo").classList.remove("error-info1")
+                }
+
+    
+                e.target[0].value=""
+                
+           
+                e.target[1].value=""       
+                
+                e.target[2].value=""   
+                  
+                openGreatModal()
+                
+        } 
+        else{
+            if(e.target[0].value.length < 1 ){
+                
+                document.querySelector("#name").classList.add("input-error")
+               document.querySelector("#errorText").classList.add("error-text1")
+            }
+            if(e.target[1].value.length < 1){
+                document.querySelector("#tel").classList.add("tel-error")
+                document.querySelector("#errorTel").classList.add("error-tel1")
+            } 
+            if(e.target[2].value.length < 1){
+                document.querySelector("#textarea").classList.add("error-info")
+                document.querySelector("#errorInfo").classList.add("error-info1")
+            }   
+        }
+      
+    }
+
+
+
     return(
-        <div className="form">
+        <div id='form' className="form">
             <div className="container">
                 <div className="form-page">
                     <h3 className="form-name">Для сотрудничества дополните форму</h3>
 
-                    <form  className="form-list">
-                        <input required type="text" placeholder='Ismingiz' className="form-input" />
-                        <input required type="text" placeholder='Telefon raqamingiz' className='form-input' />
-                        <textarea required placeholder='Xabar' className='form-text'></textarea>
-                        <button className="form-btn">Отправитъ</button>
+                    <form id="myForm" onSubmit={formBtn}  className="form-list">
+                        <label htmlFor="">
+                            <input name="name" id="name" type="text"  placeholder="Ismingizni kiriting" className=" form-input" />
+                            <p className="error-text" id='errorText'>
+                                Siz ismingizni kiritmadingiz
+                            </p>
+                        </label>
+                       <input name="tel" id="tel" type="tel"  placeholder="+998" className=" form-input" />
+                            <p className="tel-errors" id='errorTel'>
+                                Siz telefon raqamingizni kiritmadingiz
+                            </p>
+                        <textarea placeholder="Xabar" id="textarea"
+                            name="textarea" className="form-text" />
+                        <span className="info-errors" id='errorInfo'>
+                            Siz xabar kiritmadingiz
+                        </span>
+                        <button id="btnSubmit" type='submit' className="form-btn">Отправитъ</button>
                     </form>
                 </div>
             </div>
