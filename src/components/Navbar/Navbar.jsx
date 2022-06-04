78a7ca40e3dd8ef1929a4dc466f66059e170e221
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-04 18:15:50 +0500

    update files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import './Navbar.css'
-
+import { Link } from 'react-router-dom'
 import pero from '../../Assets/img/pero.png'
-import call from '../../Assets/img/calls.png'
-import toggle from '../../Assets/img/toggle.png'
-
+import call from '../../Assets/img/mp3.png'
 import Toogle from '../../SVG/Toogle'
+import button from '../../Assets/img/btn.jpg'
+import Modal from 'react-modal';
+import remove from '../../Assets/img/remove.png'
+import { AiFillCloseCircle } from 'react-icons/fa';
+
+const customStyles = {
+  content: {
+    top: '52%',
+    left: '100%',
+    right: '0%',
+    bottom: 'auto',
+    marginRight: '-100%',
+    height: '100vh',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#f2f2f2'
+  },
+};
+
+
+
+
 
 const Navbar = () =>{
 
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+  
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      subtitle.style.color = '#f00';
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
+     
 
 return(
 <div className="navbar">
@@ -24,10 +59,10 @@ return(
             </div>
             <ul className="navbar-list">
                 <li className="navbar-item">
-                    <a href="#" className="navbar-link">Главная</a>
+                    <a href="#home" className="navbar-link">Главная</a>
                 </li>
                 <li className="navbar-item">
-                    <a href="#" className="navbar-link"> О Компании</a>
+                    <a  href="#company" className="navbar-link"> О Компании</a>
                 </li>
                 <li className="navbar-item">
                     <a href="#" className="navbar-link">НoReCa</a>
@@ -36,17 +71,57 @@ return(
                     <a href="#" className="navbar-link">Tissue</a>
                 </li>
                 <li className="navbar-item">
-                    <a href="#" className="navbar-link">Контакты</a>
+                    <a href="#form" className="navbar-link">Контакты</a>
                 </li>
             </ul>
             <div className="navbar-title">
                 <img src={call} alt="" className="navbar-logos" />
-                <a href="+998911770077" className="navbar-phone">(91) 177 00 77</a>
+                <Link href="+998911770077" className="navbar-phone">(91) 177 00 77</Link>
             </div>
+            <button onClick={openModal}
+            className="navbar-btn"><img src={button} alt="" className="navbar-button" /></button>
         </div>
     </div>
 
-    {/* <a href="" className="main__button">Watch now</a> */}
+    <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button className='modal-btn' onClick={closeModal}>
+         <img src={remove} alt="" className="modal-remove" />
+        </button>
+        <ul className="modal-list">
+            <li className="modal-item">
+                <a href="#home" className="modal-link">Главная</a>
+            </li>
+            <li className="modal-item">
+                <a href="#company" className="modal-link">О Компании</a>
+            </li>
+            <li className="modal-item">
+                <a href="#" className="modal-link">НoReCa</a>
+            </li>
+            <li className="modal-item">
+                <a href="#" className="modal-link">Tissue</a>
+            </li>
+            <li className="modal-item">
+                <a href="#form" className="modal-link">Контакты</a>
+            </li>
+            <li className='modal-item modal-items'>
+                <img src={call} alt="" className="navbar-logos" />
+                <Link href="+998911770077" className="modal-phone">(91) 177 00 77</Link>
+            </li>
+            <li className="modal-item">
+                <button className="modal-titles">
+                    {/* <img src={toggle} alt="" className="" /> */}
+                    <Toogle />
+                    Наша продукция
+                </button>
+            </li>
+        </ul>
+      </Modal>
 </div>
 )
 }
