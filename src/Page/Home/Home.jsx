7257d977b876b0f2fa0467412aa40a1e5e051e05
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-04 18:16:15 +0500

    commit

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.css'
 
 import Company from '../../components/Company/Company'
+import Box from '../../components/Box/Box'
 
 const Home = () =>{
     return(
         <div className="home">
+            <Box />
             <Company />
         </div>
     )
