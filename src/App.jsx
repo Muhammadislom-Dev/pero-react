6e54f68e4d5ec76912847fca23b8708d1a89fdeb
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-04 18:13:46 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React from "react";
 import Home from "./Page/Home/Home";
 import HeaderInput from "./Page/HeaderInput/HeaderInput";
 import Section from "./components/Section/Section";
 import Rating from "./components/Rating/Rating";
 import Brand from './Page/Brand/Brand'
 import Footer from "./components/Footer/Footer";
+import './App.css'
 
 import Aos from "aos"
 import "aos/dist/aos.css"
 import  {useEffect}  from 'react'
 
-function App() {
 
+function App() {
   useEffect(() =>{
     Aos.init({});
   }, []);
 
   return (
     <div className="App">
-        <HeaderInput />
-        <Home />
-        <Section />
-        <Rating />
-        <Brand />
-        <Footer />
+          <HeaderInput />
+          <Home />
+          <Section />
+          <Rating />
+          <Brand />
+          <Footer /> 
     </div>
   );
 }
