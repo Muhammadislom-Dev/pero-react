a9a4185a6de090f4c4a87758d3dc394adc7f3465
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-04 18:13:56 +0500

    add files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './i18n';
 
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <Suspense fallback={(<div>Loading</div>)}>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </Suspense>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
